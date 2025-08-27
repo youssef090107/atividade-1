4d52c5bec3ffa431c0dc6c27a21437005a90c57d
youssef090107 <youssef.sassine2007@gmail.com>
2025-08-27 12:36:38 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+//cria referência ao form e ao elemento h3 (onde será exibida a resposta)
+const frm = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+frm.addEventListener("submit", (e) => {
+  const nome =
+
+})
